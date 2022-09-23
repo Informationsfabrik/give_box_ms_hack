014db42bf982d233b30c60490651ffca966650a9
Leroy Anozie <lanozie@informationsfabrik.de>
2022-09-23 15:57:43 +0200

    added utils and updated box.py

--- fastapi-backend/routers/box.py
+++ fastapi-backend/routers/box.py
@@ -2,27 +2,49 @@ from datetime import datetime
 from fastapi import APIRouter, Depends
 from fastapi_jwt_auth import AuthJWT
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+from sqlalchemy.orm import Session
+from utils import get_db
+import models
 
 router = APIRouter()
 
 
-class Box(BaseModel):
+class GiveBoxBase(BaseModel):
     id: int
     longitide: float
     latitude: float
     address: str
-    temporary: bool
-    name: str
+    is_temporary: bool
+    description: str
+    opening_hours: str
+
+
+class GiveBox(GiveBoxBase):
     last_checked: datetime
     maintenance_needed: bool
     maintainer_information: Optional[str]
+    last_confirmation_date: datetime
     # maintainer
-    # öffnungszeiten
 
 
-@router.get("/box/{id_}")
-def items(id_: int, Authorize: AuthJWT = Depends()) -> Box:
+@router.get("/givebox/{id_}")
+def get_givebox_by_id(
+    id_: int, db: Session = Depends(get_db), Authorize: AuthJWT = Depends()
+) -> GiveBox:
     Authorize.jwt_required()
 
-    return {"items": items}
+    box = db.query(models.GiveBox).filter(models.GiveBox.id == id_).first()
+
+    return GiveBox(box)
+
+
+@router.get("/givebox")
+def get_givebox_list(
+    db: Session = Depends(get_db), Authorize: AuthJWT = Depends()
+) -> List[GiveBoxBase]:
+    Authorize.jwt_required()
+
+    box = db.query(models.GiveBox)
+
+    return GiveBox(box)
