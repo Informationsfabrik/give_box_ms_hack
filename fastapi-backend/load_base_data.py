190560732e1e4675bb1d31241edf2e8dcbc9cf5c
Rainer Mensing <rainer.mensing@hotmail.de>
2022-09-24 14:59:43 +0200

    Update base data

--- fastapi-backend/load_base_data.py
+++ fastapi-backend/load_base_data.py
@@ -1,22 +1,32 @@
 import requests
 import json
+import os
+
+try:
+    if os.environ['ENV'] == 'prod':
+        server = 'https://api.givebox-ms.de/'
+    else:
+        server = 'http://localhost:8081/'
+except:
+    server = 'http://localhost:8081/'
 
 
-def load_base_data(router):
+
+def load_base_data(router,server = server):
     with open("data/"+router+".json") as json_file:
         data = json.load(json_file)
         for item in data:
-            requests.post('http://127.0.0.1:8081/'+router, json=item)
+            requests.post(server+router, json=item)
 
-#load_base_data("giveboxes")
+load_base_data("giveboxes")
 
-#load_base_data("users")
+load_base_data("users")
 
 
-def load_demo_maintainer(router):
+def load_demo_maintainer(router,server = server):
     with open("data/"+router+".json") as json_file:
         data = json.load(json_file)
         for item in data:
-            requests.post('http://127.0.0.1:8081/'+router+'/'+str(item['box_id'])+'/'+str(item['user_id']))
+            requests.post(server+router+'/'+str(item['box_id'])+'/'+str(item['user_id']))
 
 load_demo_maintainer("add_maintainer")
