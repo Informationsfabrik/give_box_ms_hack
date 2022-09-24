b9efb72db8301be6270492fbcd5e3a036e38e36e
lbrentrup <lbrentrup@informationsfabrik.com>
2022-09-24 15:39:51 +0200

    Add image persistance

--- svelte-frontend/src/lib/private-box/PrivateBox.svelte
+++ svelte-frontend/src/lib/private-box/PrivateBox.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { ENV_OBJ } from '$lib/env'
+	// import ImageUpload from "$lib/imageupload/ImageUpload.svelte";
 	let longitude;
 	let latitude;
 	let is_temporary = true;
@@ -25,7 +26,18 @@
 	let others = "";
 
 	let box = 100;
+	let image;
 
+	let avatar;
+	let fileinput;
+	const onFileSelected =(e)=>{
+		let image = e.target.files[0];
+		let reader = new FileReader();
+		reader.readAsDataURL(image);
+		reader.onload = e => {
+			avatar = e.target.result
+		};
+	}
 
 	async function submit() {
 
@@ -60,29 +72,53 @@
 			body: JSON.stringify(dict),
 			headers: {'mode':'no-cors', 'content-type': 'application/json'}
 		}).then((res) => res.json())
+
+		var image_dict = {
+			"box_id": Number(box),
+			"is_title_image": true,
+			"data" : avatar
+		}
+		image = await fetch(ENV_OBJ.API_URL + '/images', {
+			method: 'POST',
+			body: JSON.stringify(image_dict),
+			headers: {'mode':'no-cors', 'content-type': 'application/json'}
+		}).then((res) => res.json())
+
 	}
 
 </script>
 
 {#if box===100}
-<form>
-	<input bind:value="{longitude}" placeholder="Longitude">
-	<input bind:value="{latitude}" placeholder="Latitude">
-	<input bind:value="{description}" placeholder="Beschreibung">
-	<input bind:value="{opening_hours}" placeholder="Öffnungszeiten">
-	<input bind:value="{street}" placeholder="Staße">
-	<input bind:value="{house_number}" placeholder="Hausnummer">
-	<input bind:value="{zip_code}" placeholder="Postleitzahl">
-	<input bind:value="{city}" placeholder="Stadt">
+	<h1>Erstelle deine Box</h1>
+	<div id="app">
+		<h1>Upload Image</h1>
 
-	<label>Bücher<input type="checkbox" on:change={(e) => {book =e.target.checked}}></label>
-	<label>Kleidung<input type="checkbox" on:change={ (e) => {clothes =e.target.checked}}></label>
-	<label>Elektronik<input type="checkbox" on:change={(e) => {electronics =e.target.checked}}></label>
-	<label>Spielzeug<input type="checkbox" on:change={ (e) => {toys =e.target.checked}}></label>
-	<input bind:value="{others}" placeholder="Sonstiges">
+		{#if avatar}
+			<img class="avatar" src="{avatar}" alt="d" />
+		{:else}
+			<p>No image uploaded</p>
+		{/if}
+		<button class="upload" on:click={()=>{fileinput.click();}}> Image upload </button>
+		<input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
+	</div>
+	<form>
+		<input bind:value="{longitude}" placeholder="Longitude">
+		<input bind:value="{latitude}" placeholder="Latitude">
+		<input bind:value="{description}" placeholder="Beschreibung">
+		<input bind:value="{opening_hours}" placeholder="Öffnungszeiten">
+		<input bind:value="{street}" placeholder="Staße">
+		<input bind:value="{house_number}" placeholder="Hausnummer">
+		<input bind:value="{zip_code}" placeholder="Postleitzahl">
+		<input bind:value="{city}" placeholder="Stadt">
 
-</form>
-<button on:click={submit}>Submit</button>
+		<label>Bücher<input type="checkbox" on:change={(e) => {book =e.target.checked}}></label>
+		<label>Kleidung<input type="checkbox" on:change={ (e) => {clothes =e.target.checked}}></label>
+		<label>Elektronik<input type="checkbox" on:change={(e) => {electronics =e.target.checked}}></label>
+		<label>Spielzeug<input type="checkbox" on:change={ (e) => {toys =e.target.checked}}></label>
+		<input bind:value="{others}" placeholder="Sonstiges">
+
+	</form>
+	<button on:click={submit}>Submit</button>
 {:else if box === 200}
 	<h1>Thank you</h1>
 	<a href="/">Back</a>
@@ -92,5 +128,21 @@
 	Input, button{
 		display:block;
 		margin:10px;
+	}
+	#app{
+		display:flex;
+		align-items:center;
+		justify-content:center;
+		flex-flow:column;
+	}
+
+	.upload{
+		display:flex;
+		cursor:pointer;
+	}
+	.avatar{
+		display:flex;
+		height:200px;
+		width:200px;
 	}
 </style>
