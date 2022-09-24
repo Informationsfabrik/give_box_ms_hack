76e78b05f5e50b490dd51598387ab37e979ec34e
Leschek Homann <leschek.homann@ikor.de>
2022-09-24 13:14:40 +0200

    Drop address_id

--- fastapi-backend/transform_to_json.py
+++ fastapi-backend/transform_to_json.py
@@ -18,15 +18,15 @@ def transform_to_json(file_name):
     data_frame = pd.read_csv(f"./fastapi-backend/data/{file_name}", sep=";")
 
     data_frame.rename(columns=MAPPING, inplace=True)
-    data_frame.drop(columns=["Typ"], inplace=True)
-
-    data_frame[["street", "house_number"]] = data_frame["address_id"].str.extract(r'(\w.*) (\d{2})', expand=True)
+    data_frame[["street", "house_number"]] = data_frame["address_id"].str.extract(r'(\w.*) (\d{2}),', expand=True)
     data_frame[["zip_code", "city"]] = data_frame["address_id"].str.extract(r'(\d{5}) (\w.*)', expand=True)
     data_frame["image_id"] = [image_id for image_id in range(data_frame.shape[0])]
     data_frame["content"] = [{"books": True} for _ in range(data_frame.shape[0])]
     data_frame["is_temporary"] = [False for _ in range(data_frame.shape[0])]
     data_frame["maintenance_needed"] = [False for _ in range(data_frame.shape[0])]
     data_frame["maintainer_info"] = ["" for _ in range(data_frame.shape[0])]
+
+    data_frame.drop(columns=["Typ", "address_id"], inplace=True)
 
     json_data = json.loads(data_frame.to_json(orient="records"))
     json.dump(json_data, open("test.json","w"), indent=4)
