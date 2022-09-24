6d0b7e547ba953f777ab011d47501147056ebf68
Leschek Homann <leschek.homann@ikor.de>
2022-09-24 13:06:33 +0200

    Updated script

--- fastapi-backend/transform_to_json.py
+++ fastapi-backend/transform_to_json.py
@@ -20,10 +20,8 @@ def transform_to_json(file_name):
     data_frame.rename(columns=MAPPING, inplace=True)
     data_frame.drop(columns=["Typ"], inplace=True)
 
-    data_frame["street"] = "ToDo"
-    data_frame["house_number"] = data_frame["address_id"].str.extract(r'(\d{2})', expand=True)
-    data_frame["city"] = "ToDo"
-    data_frame["zip_code"] = data_frame["address_id"].str.extract(r'(\d{5})', expand=True)
+    data_frame[["street", "house_number"]] = data_frame["address_id"].str.extract(r'(\w.*) (\d{2})', expand=True)
+    data_frame[["zip_code", "city"]] = data_frame["address_id"].str.extract(r'(\d{5}) (\w.*)', expand=True)
     data_frame["image_id"] = [image_id for image_id in range(data_frame.shape[0])]
     data_frame["content"] = [{"books": True} for _ in range(data_frame.shape[0])]
     data_frame["is_temporary"] = [False for _ in range(data_frame.shape[0])]
